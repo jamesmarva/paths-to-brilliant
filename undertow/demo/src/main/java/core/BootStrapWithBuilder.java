29da835362314a8241155f6c3b9041fd0d79dbe7
jameskk <jamesmarva@163.com>
2021-01-01 09:43:30 +0800

    feat: 新增undertow项目

---
+++ undertow/demo/src/main/java/core/BootStrapWithBuilder.java
@@ -0,0 +1,14 @@
+package core;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @author 王涵威
+ * @date 20.12.31 12:17
+ */
+public class BootStrapWithBuilder {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ undertow/demo/src/main/java/reverseproxy/ReverseProxyServer.java
@@ -0,0 +1,37 @@
+package reverseproxy;
+
+import io.undertow.Undertow;
+import io.undertow.server.handlers.proxy.LoadBalancingProxyClient;
+import io.undertow.server.handlers.proxy.ProxyHandler;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ *
+ *
+ *
+ * @author 王涵威
+ * @date 21.1.1 8:56
+ */
+public class ReverseProxyServer {
+
+    public static void main(String[] args) throws URISyntaxException {
+        LoadBalancingProxyClient loadBalancingProxyClient  = new LoadBalancingProxyClient()
+                .addHost(new URI("http://localhost:8081"))
+                .addHost(new URI("http://localhost:8082"))
+                .addHost(new URI("http://localhost:8083"))
+                .setConnectionsPerThread(20);
+        Undertow proxyServer = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setIoThreads(4)
+                .setHandler(ProxyHandler.builder()
+                        .setProxyClient(loadBalancingProxyClient)
+                        .setMaxRequestTime(30000)
+                        .build())
+                .build();
+        proxyServer.start();
+
+
+    }
+}
